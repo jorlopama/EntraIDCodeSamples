41e12c3be0ebe2dfa2fa488213c9c908b5a7e786
Jorge Lopez <43187678+jorlopama@users.noreply.github.com>
2024-02-13 17:34:19 -0500

    Added new Files

---
+++ AddPermissionToManagedIdentity.ps1
@@ -0,0 +1,13 @@
+Install-Module Microsoft.Graph -Scope CurrentUser
+
+Connect-MgGraph -Scopes "User.readWrite.all"
+#Select-MgProfile Beta
+$graphApp = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
+
+$PermissionName = "User.ReadWrite.All"
+$AppRole = $graphApp.AppRoles | `
+Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+#Below Line needs to be modifed in order to give access to the ManagedIdentity of the Logic App
+$managedID = Get-MgServicePrincipal -Filter "DisplayName eq '<<logiAppName>>'"
+New-MgServicePrincipalAppRoleAssignment -PrincipalId $managedID.Id -ServicePrincipalId $managedID.Id -ResourceId $graphApp.Id -AppRoleId $AppRole.Id
+
